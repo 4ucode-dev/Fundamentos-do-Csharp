5031c02879e949b98c4670fba43644c297029214
Guilherme Cardoso <guilhermecardosor.2004@gmail.com>
2023-07-06 13:44:38 -0300

    Aula 04

---
+++ Calculadora - Aula 04/Aula 04.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula04
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
