9467d6197e52400bd56011241ef5e91f48b782cf
Guilherme Cardoso <guilhermecardosor.2004@gmail.com>
2023-07-06 12:36:46 -0300

    Aula-02

---
+++ Calculadora - Aula 02/Aula 02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+
+            Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            Console.WriteLine("O resultado da soma é " + (v1 + v2));
+            Console.WriteLine("O resultado da soma é " + v1 + v2);
+        }
+    }
+}
