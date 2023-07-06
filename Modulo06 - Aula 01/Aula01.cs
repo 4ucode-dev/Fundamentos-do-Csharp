2652e4e9b6d7ca9337003ba927cac1402cbb5e9d
Guilherme Cardoso <guilhermecardosor2004@gmail.com>
2023-07-05 21:09:48 -0300

    Primeira Aula

---
+++ Modulo06 - Aula 01/Aula01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor : ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
+
+
+
+
