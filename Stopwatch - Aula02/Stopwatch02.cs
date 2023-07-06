4fa5c66f3e2e0f4020133a529ef524a6f85687a7
Guilherme Cardoso <guilhermecardosor2004@gmail.com>
2023-07-06 20:59:07 -0300

    Stopwatch02

---
+++ Stopwatch - Aula02/Stopwatch02.cs
@@ -0,0 +1,24 @@
+﻿namespace Stopwatch02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
