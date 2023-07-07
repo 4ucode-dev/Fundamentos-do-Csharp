8a5cd398c42b069ff84408dd6f473028f612f2d8
Guilherme Cardoso <guilhermecardosor2004@gmail.com>
2023-07-06 21:23:02 -0300

    Stopwatch04

---
+++ Stopwatch - Aula04/Stopwatch04.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 Segundos");
+            Console.WriteLine("M - Minuto => 1m = 1 Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            Console.WriteLine(data);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch Finalizado!");
+            Thread.Sleep(2500);
+        }
+    }
+}
